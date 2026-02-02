4ef07b28932d9ca21faa93bc46a296bce8b98d67
angelbrisenio31 <angelbrisenio31@gmail.com>
2026-02-02 10:48:09 +0800

    Initial commit by Student Brisenio.

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-print ("Lab 1: Version Contol")
-print ("Author:[MA. ANGELA C. BRISENIO]")
-print ("Student ID: TUPM-25-3695")
 print ("Lab 1: Version Control")
 print ("Author:MA. Angela C. Brisenio")
 print ("Student ID: TUPM-25-3695")
-print ("Class Section: [1C]")
